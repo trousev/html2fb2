32cb8446c57600b2f3963e12c20fa8af5a7a2d78
clach04 <clach04@92af5819-8937-0410-97eb-dd92dc655d52>
2007-10-17 04:29:10 +0000

    Added any2fb script which supports; html, rtf, .lit (via ConvertLIT), and plain text. Al file types except .lit can be inside a .zip file.
    git-svn-id: http://html2fb.googlecode.com/svn/trunk@34 92af5819-8937-0410-97eb-dd92dc655d52

---
+++ any2fb.py
@@ -0,0 +1,188 @@
+# -*- coding: ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+
+"""
+Any to FictionBook2 converter.
+Primarily a HTML to FictionBook2 converter, supports; .html, .rtf, .lit, and .txt
+
+Usage: %prog [options] args
+    -i, --input-file=FILENAME: (*.html|*.htm|*.html|*.rtf|*.zip|*.lit|*.*) REQUIRED Input file name
+    -o, --output-file=FILENAME: (*.fb2|*.*) Output file name, if left blank creates name based on input filename
+    -f, --encoding-from=ENCODING_NAME:  Source encoding, autodetect if omitted.
+    -t, --encoding-to=ENCODING_NAME DEFAULT=us-ascii:    Encoding to use in final fb2 book.
+    --convert_span_to=CHOICE(em|strong|remove) DEFAULT=em: What to convert html <span> tags to
+    --convert-quotes:     Convert quotes, i.e. convert "" to << >>
+    --convert-hyphen:     Convert hyphens, i.e. - to ndash
+    --skip-images:        Skip images, i.e. if specified do NOT include images.
+    --convert-images:     Always convert images to PNG
+    --no_centered_to_headers: (RTF input only) Do not convert centered text into (html)headers/(FB)titles. By default RTF centered text is converted into headers/titles
+    --no_zip_output:    Do not create compressed ebook. By default output files are put in a zip file (to save space)
+    --convertlit=FILENAME: (*.exe|*.*) (LIT input only) Filename for ConvertLIT executable (OPTIONAL, only needed for converting .LIT format, see www.convertlit.com)
+    --no_clean_temp_lit: (LIT input only) Do not remove/delete intermediate exploded .lit files
+    --aggressive_lit_html_clean: (LIT input only) Aggressive HTML clean for .lit source files (WARNING may loose proper tags!)
+"""
+
+import cgitb
+#cgitb.enable(format="text") 
+
+import sys
+
+import optionparse
+#import weboptionparse as optionparse
+import h2fb
+try:
+    #raise ImportError
+    import compressedfile
+except ImportError:
+    compressedfile=None
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    opt, args = optionparse.parse(__doc__, argv[1:])
+
+    params = h2fb.default_params.copy()
+    
+    # Process command line options only used by this module
+    rtf_centered_to_headers = opt.no_centered_to_headers != True
+    del opt.no_centered_to_headers
+    no_zip_output = opt.no_zip_output == True
+    del opt.no_zip_output
+    convertlit_path = opt.convertlit
+    del opt.convertlit
+    lit_clean_temp_dir = opt.no_clean_temp_lit != True
+    del opt.no_clean_temp_lit
+    aggressive_lit_html_clean = opt.aggressive_lit_html_clean == True
+    del opt.aggressive_lit_html_clean
+    
+    # Convert out command line options into something h2fb can handle
+    
+    # convert underscores to hypens,
+    # e.g. "option_something" in to "option-something" (which h2fb expects)
+    opt_dict={}
+    for temp_param in dir(opt):
+        temp_value = getattr(opt, temp_param)
+        if not temp_param.startswith('_') and not callable(temp_value):
+            temp_key = temp_param.replace('_', '-') ## horrible hack! as "-" were turned into '_' in the argument processor
+            if isinstance(temp_value, unicode):
+                temp_value=str(temp_value)
+            opt_dict[temp_key] = temp_value
+    
+    in_filename = opt_dict['input-file']
+    del opt_dict['input-file']
+    out_filename = opt_dict['output-file']
+    del opt_dict['output-file']
+    if not out_filename:
+        out_filename = in_filename + '_' + opt_dict['encoding-to'] + '.fb2' ## dumb but quick
+
+    # Handle .lit files
+    isLIT=False
+    ## FIXME clean up temp directory at end!!
+    if in_filename.lower().endswith('.lit'):
+        isLIT=True
+    if isLIT:
+        from litfuncs import ExtractLIT, clean_lit_str, del_dir, experimental_clean_lit_str
+        
+        lit_clean_html = clean_lit_str
+        if aggressive_lit_html_clean:
+            lit_clean_html = experimental_clean_lit_str
+        ##FIXME get temp dir return... and use it later for clean up
+        temp_lit_directory = out_filename + '__temp_lit_DIR'
+        temp_html_filename = ExtractLIT(in_filename, temp_lit_directory, clit_exe=convertlit_path)
+        print 'temp_html_filename', temp_html_filename
+        in_filename = temp_html_filename
+        # FIXME should be able to dectect this (in h2b??)
+        #params['encoding-from'] = 'UTF-8'
+
+
+    params['file-name'] = in_filename
+    # Simply overwrite dictionary
+    for temp_param in opt_dict:
+        params[temp_param] = opt_dict[temp_param]
+    
+    params['detect-verses'] = 0 ## DEBUG, workaround for bug in detect-verses code (line length??), detect-verses makes NO sense for html input files, only makes sense for raw text files (h2fb can handle plain text files)
+    params['skip-empty-lines'] = 0
+    
+    print 'Input file:', in_filename 
+    print 'Output file:', out_filename
+
+    if compressedfile is None:
+        myfile_open = open
+    else:
+        myfile_open = compressedfile.open_zipfile
+    if compressedfile is None or no_zip_output:
+        myfile_open_for_write = open
+    else:
+        myfile_open_for_write = compressedfile.open_zipfile
+
+    # start actually processing the file now
+    
+    in_file = myfile_open(in_filename, 'rb')
+    out_file = myfile_open_for_write(out_filename, 'w')
+    
+    input_text = in_file.read()
+    if isLIT:
+        # .lit (html) input stream clean up
+        input_text = lit_clean_html(input_text)
+    #### DEBUG
+    #~ print '-'*65
+    #~ print repr(input_text[:24])
+    #~ print '-'*65
+    #~ print '='*65
+    #~ input_text = unicode(input_text,'utf8')
+    #~ print '*'*65
+    #### DEBUG
+    if input_text.startswith('{\\rtf'):
+        # Looks like we have RTF data not html!
+        import rtf.Rtf2Html
+        #### START temp rtf workaround until Rtf2Html suports e?dash and optional hyphen
+        # RTF input stream clean up
+        ## See bug http://sourceforge.net/tracker/index.php?func=detail&aid=1811128&group_id=103452&atid=634851
+        ## older rtf spec http://latex2rtf.sourceforge.net/RTF-Spec-1.0.txt
+        
+        # Newline removal ends up without any empty paragraphs (i.e. empty-line tags in FB2)
+        # Fits in with what the input RTF file looks like so I think this is the RIGHT thing to do until RTF lib is fixed.
+        # Also fixes missing bold tags in _some_ cases....
+        input_text = input_text.replace('\r\n','') # remove newlines...
+        input_text = input_text.replace('\n','') # remove newlines...
+        ## TODO remove MAC newlines.....??
+        for x in ['\emdash', '\\endash']:
+            input_text = input_text.replace(x+' ','-')
+            input_text = input_text.replace(x,'-')
+        input_text = input_text.replace('\\-\n','') # remove, could replace...
+        input_text = input_text.replace('\\-\r\n','') # remove, could replace...
+        input_text = input_text.replace('\\-','') # remove, could replace...
+        #### END temp rtf workaround until Rtf2Html suports e?dash and optional hyphen
+        input_text = rtf.Rtf2Html.getHtml(input_text)
+        ## TODO add Centered (or bold) tags as headers?
+        if rtf_centered_to_headers:
+            ## FIXME use beautiful soup (http://www.crummy.com/software/BeautifulSoup/) to find, '<div align='center'>' and convert to H2
+            input_text = input_text.replace("<div align='center'>",'<h2>')
+            input_text = input_text.replace('</div>','</h2>')
+            
+    ## TODO add beautiful soup (http://www.crummy.com/software/BeautifulSoup/) clean html before convert support? Possibly put that into h2fb? may help with bad nested bold tags (see unittest, test_nested_emphasis_italic_lesssimple)
+    ## TODO add PDF support??
+    ## TODO add Table Of Contents generation - if using a decent reader like Haalireader this is not needed.
+    ## TODO zip'd support of html archives that contain images
+    ## BUGFIX filenames in zip's - needed to ensure internal href's are translated correctly, especially for ./thisfile.html#link_name style href's
+    
+    params['data'] = input_text
+    in_file.close()
+    data=h2fb.MyHTMLParser().process(params)
+    out_file.write(data)
+    out_file.close()
+    
+    if isLIT:
+        # .lit temp file clean up
+        if lit_clean_temp_dir:
+            del_dir(temp_lit_directory)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
---
+++ litfuncs.py
@@ -0,0 +1,267 @@
+# -*- coding: ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+"""wrapper around ConvertLIT http://www.convertlit.com/
+this is simple  a wrapper around clit.exe NOT a library wrapper
+
+Uses python 2.4 subprocess module http://docs.python.org/lib/module-subprocess.html
+"""
+
+import os
+import re
+
+import subprocess
+
+class ConvertLITError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
+def del_dir(path):
+    if not os.path.exists(path):
+        return
+    for file in os.listdir(path):
+        file_or_dir = os.path.join(path,file)
+        if os.path.isdir(file_or_dir) and not os.path.islink(file_or_dir):
+            del_dir(file_or_dir) #it's a directory reucursive call to function again
+        else:
+            #os.remove(file_or_dir) #it's a file, delete it
+            try:
+                os.remove(file_or_dir) #it's a file, delete it
+            except OSError, info:
+                #probably failed because it is not a normal file or permissions problem
+                if info.errno != 2:
+                    print OSError, info.errno, info, dir(info)
+                    raise
+                    ## FIXME change perms....
+                    os.remove(file_or_dir) #it's a file, delete it
+                else:
+                    pass
+    os.removedirs(path) #delete the directory here
+
+def safe_mkdir(newdir):
+    result_dir = os.path.abspath(newdir)
+    try:
+        os.makedirs(result_dir)
+    except OSError, info:
+        if info.errno == 17 and os.path.isdir(result_dir):
+            pass
+        else:
+            raise
+
+def find_files_with_extension(directory_name, file_extension_list):
+    # FIXME only does single dir (does not recurse)
+    local_file_extension_list = [file_extension.lower() for file_extension in file_extension_list] 
+    #file_extension = file_extension.lower()
+    content = os.listdir(directory_name)
+    temp_list = []
+    for temp_file in content:
+        for file_extension in local_file_extension_list:
+            if temp_file.lower().endswith(file_extension):
+                full_path = os.path.join(directory_name, temp_file)
+                temp_list.append((os.path.getsize(full_path ), full_path ))
+                break
+    return  temp_list
+
+def find_main_lit_file(directory_name):
+    directory_name = os.path.abspath(directory_name)
+    
+    ### Dumb Brute Force find index.html (or derivatives)
+    ## want case insensitive glob....
+    ##index_filename = os.path.join(directory_name, '.html
+    ##os.path.exists()
+    main_filename = None
+    content = os.listdir(directory_name)
+    for x in content:
+        if x.lower() == 'index.html' or x.lower() == 'index.htm':
+            main_filename = os.path.join(directory_name, x)
+            break
+    
+    if main_filename is None:
+        # Find largest html file
+        file_list = find_files_with_extension(directory_name, ['.htm', '.html'] )
+        file_list.sort()
+        ## get the filename of the last (largest size) entry
+        """
+        file_list.reverse() # yeah I know this is bad... but works with pre-Python 2.4
+        main_filename = file_list[0][1] ## FIXME just get file_list[-1][1]  and loose the reverse above
+        """
+        main_filename = file_list[-1][1]
+    return main_filename
+    
+
+
+def ExtractLIT(in_filename, out_dirname=None, clit_exe=None):
+    """Extract files from .lit file and return best guess main html file
+    TODO should it return directory too, i.e. make out_dirname optional and default it if not specified?"""
+    in_filename = os.path.abspath(in_filename)
+    if out_dirname is None:
+        out_dirname=in_filename+'_DIR'
+    if clit_exe is None:
+        clit_exe=CLIT_EXE
+    if not os.path.exists(clit_exe):
+        raise ConvertLITError('Missing ConvertLIT exe %r, download from http://www.convertlit.com'% clit_exe)
+
+    out_dirname = os.path.abspath(out_dirname)
+    out_dirname = os.path.join(out_dirname, '') # add trailing slash to out_dirname  to prevent clit from crashing...
+
+    print 'ExtractLIT: in_filename', repr(in_filename )
+    print 'ExtractLIT: out_dirname', repr(out_dirname)
+
+    del_dir(out_dirname)
+    #safe_mkdir(out_dirname) ###  not needed for Windows, is needed for Linux??
+    ## what about ConvertLIT stdio and stderr...?
+    retcode = subprocess.call([clit_exe, in_filename, out_dirname])
+    print 'ExtractLIT: retcode', retcode
+
+    if retcode != 0:
+        raise ConvertLITError('subprocess call failed! retcode=%d'% retcode)
+
+    return find_main_lit_file(out_dirname)
+
+
+#####################
+# Simple key words not tag based :-(
+bad_values = """
+<![if>
+</![if>
+<![endif]>
+</![endif]>
+<o:p>
+</o:p>
+<st1>
+</st1:place>
+</st1:placename>
+</st1:placetype>
+</st1:address>
+</st1:street>
+</st1:city>
+</st1:state>
+</st1:country-region>
+"""
+
+## TODO FIXME add:
+"""
+<st1*> -- i.e. wildcarded
+"""
+
+
+bad_values_list = []
+for x in bad_values.split('\n'):
+    if x != '':
+        bad_values_list.append(x)
+
+def clean_lit_str(in_str):
+    """Clean up "bad" html that is present in various .lit xml files
+    """
+    return_str = in_str
+    # clean values found in http://www.idpf.org/ / http://openebook.org files
+    for x in bad_values_list:
+        return_str = return_str.replace(x, '')
+    return return_str
+
+###################
+
+def find_tags(in_str):
+    # Good known tags....
+    ## TODO: consider grabbing a list from w3,
+    ##       e.g. http://www.w3.org/TR/html401/index/elements.html
+    ignore_tags = [
+'body',
+'head',
+'meta',
+'html',
+'style',
+'title',
+'body',
+'br',
+'sub',
+'sup',
+'h1',
+'h2',
+'h3',
+'h4',
+'h5',
+'h6',
+'h7', # ???
+'b',
+'i',
+'a',
+'p',
+'span',
+'div',
+]
+
+    findtags_re = r"""(?P<tag><.*?>)"""
+    findtags_reobj = re.compile(findtags_re,  re.IGNORECASE| re.DOTALL)
+
+    #findtagname_re = r"""</?(?P<tag_name>.*?)>"""
+    #findtagname_re = r"""</?(?P<tag_name>\w+).*>"""
+    findtagname_re = r"""</?(?P<tag_name>\S*).*>"""
+    findtagname_reobj = re.compile(findtagname_re,  re.IGNORECASE| re.DOTALL)
+
+    """
+    for x in findtags_reobj.findall(in_str):
+        print x
+    print '-'*65
+    """
+    tag_counts = {}
+    for x in findtags_reobj.finditer(in_str):
+        temp_html_tag = x.group('tag')
+        #print temp_html_tag
+        """
+        if not temp_html_tag.startswith('<?xml version="1.0"') and not temp_html_tag.startswith('<!DOCTYPE'):
+            #print temp_html_tag,
+            #print findtagname_reobj.search(temp_html_tag).group('tag_name')
+            temp_tagname = findtagname_reobj.search(temp_html_tag).group('tag_name')
+        else:
+            temp_tagname=temp_html_tag
+        if temp_tagname not in ignore_tags:
+            tag_counts[temp_tagname] = tag_counts.setdefault(temp_tagname, 0) + 1
+        """
+        if temp_html_tag.startswith('<?xml version="1.0"') or temp_html_tag.startswith('<!DOCTYPE'):
+            temp_tagname=temp_html_tag
+        else:
+            temp_tagname = findtagname_reobj.search(temp_html_tag).group('tag_name')
+            if temp_tagname not in ignore_tags:
+                tag_counts[temp_tagname] = tag_counts.setdefault(temp_tagname, 0) + 1
+    return tag_counts
+
+def tag_count_sort(tag_counts):
+    sorted_tag_counts = []
+    for x in tag_counts:
+        sorted_tag_counts.append((tag_counts[x], x))
+    sorted_tag_counts.sort()
+    return sorted_tag_counts
+
+def remove_unknown_tags(in_str, remove_tag_list):
+    """Clean up "bad" html b removing uunknown tags
+    """
+    return_str = in_str
+    done_tags = []
+    for tag_count, tag_name in remove_tag_list:
+        ## FIXME do a pass for tags with colons in them (remove after and including colon)
+        if ':' in tag_name:
+            tag_name = tag_name[:tag_name.find(':')]
+            if tag_name in done_tags:
+                continue
+            done_tags.append(tag_name)
+        temp_re = """</?%s.*?>""" % re.escape(tag_name)
+        #print temp_re
+        temp_compile_obj = re.compile(temp_re,  re.IGNORECASE| re.DOTALL)
+        return_str = temp_compile_obj.sub('', return_str)
+    return return_str
+
+def experimental_clean_lit_str(html_str):
+    """Agressive, find tags that we don't recognise and remove them.
+    WARNING! Could remove good tags if "good tag" list is incomplete (which it is)
+    """
+    tag_count = find_tags(html_str)
+    sorted_tag_counts = tag_count_sort(tag_count)
+    
+    #from pprint import pprint
+    #pprint(sorted_tag_counts)
+    
+    clean_html = remove_unknown_tags(html_str, sorted_tag_counts)
+    return clean_html
